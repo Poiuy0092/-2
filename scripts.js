772e472893617d8c652a978a48db97654a7adb62
Poiuy0092 <61450514+Poiuy0092@users.noreply.github.com>
2023-10-11 13:25:42 +0800

    easytesting3

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function animationFunctions() {
+                 
+                document.querySelector('#tracks-clicktext').addEventListener('click', func31Frame30)
+                
+                function func31Frame30() {document.querySelector('#tracks-clicktext-0').classList.add('class-click--31-Frame3--31-Frame3-513-0');document.querySelector('#tracks-clicktext').classList.add('class-click--31-Frame3--31-Frame3-513');document.querySelector('#id-Frame3').classList.add('class-click--31-Frame3-Frame3-513');document.querySelector('#id-2-31-Frame3-0').classList.add('class-click--31-Frame3-2-31-Frame3-513-0');document.querySelector('#id-2-31-Frame3').classList.add('class-click--31-Frame3-2-31-Frame3-513');document.querySelector('#id-1-31-Frame3-0').classList.add('class-click--31-Frame3-1-31-Frame3-513-0');document.querySelector('#id-1-31-Frame3').classList.add('class-click--31-Frame3-1-31-Frame3-513');document.querySelector('#tracks-clicktext').removeEventListener('click', func31Frame30);};} animationFunctions();
